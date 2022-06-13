516102004797ab53c8f02de22f625f36c8154eb7
Logan S <31578557+lxsavage@users.noreply.github.com>
2022-06-12 23:47:01 -0400

    Added missing updater script (not tested fully yet)

---
+++ updater.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set _curr_cd="$(pwd)"
+# Git clone the current version of the repo to a temp directory
+echo "Fetching newest version..."
+cd $HOME
+git clone -q --depth 1 https://github.com/lxsavage/aipm.git
+
+# Run upgrade script
+echo "Running upgrade scripts..."
+cd aipm
+./upgrade.sh
+
+# Remove local clone of current version
+cd $_curr_cd
+rm -rf $HOME/aipm
+echo "Update complete!"
